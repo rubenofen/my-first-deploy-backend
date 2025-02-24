d50b095042619afc4cecb125efca3d5365e6cd9b
rubenofen <rubenofen@gmail.com>
2025-02-24 19:13:40 +0000

    new end-point

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/health", (req, res) => {
+  res.send("The service is on line");
+});
+
 app.listen(3000, () => {
   console.log("Server working on port 3000");
 });
