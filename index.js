320296305f113305bf320544d1b652dfbd91be80
rubenofen <rubenofen@gmail.com>
2025-02-24 19:32:13 +0000

    add nimbel origin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://nuclio.nimbel.net"],
 };
 
 app.use(cors(corsOptions));
